1fcc2873e0645bc76d72444236cd5ead185299f5
nvitale13 <84364602+nvitale13@users.noreply.github.com>
2021-06-22 03:07:57 +0000

    updated

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-float interestrate;
-float loanamount;
+float interestrate = 0.0;
+float loanamount = 0.0;
 
 int main() {
 
@@ -14,19 +14,28 @@ cin >> interestrate;
 cout << "Enter loan amount: ";
 cin >> loanamount;
 
-if (interestrate < 1 || interestrate >= 18)
-  if (loanamount <= 100 || loanamount >= 1000)
-    cout << "Error! "; 
-
-if (interestrate >= 1 && interestrate <= 18)
-  if (loanamount >= 100 && loanamount <= 500)
-    cout << "Fee $20 "; 
-
-if (interestrate >= 1 && interestrate <= 18)
-  if (loanamount >= 500)
-    cout << "Fee $25 ";
-
+if (interestrate < 1 || interestrate > 18){
+ cout << "Error! Interest rates are invalid. "; 
 
 return 0;
+}
+
+if (loanamount < 100 || loanamount > 1000){
+  cout << "Error! Loan rates are invalid. "; 
+
+return 0;
+}
+
+float loan_fee = 0.0;
+if (loanamount >= 100 && loanamount <= 500){
+  loan_fee = 20;
+} else 
+  loan_fee = 25;
+ 
+float total_interest = loanamount * (interestrate / 100);
+
+cout << "Loan amount $ " << loanamount << "\n";
+cout << "Interest rate: " << interestrate << "\n";
+cout << "Intrest + Fees: $ " << (loan_fee + total_interest) << "\n";
 
 }
